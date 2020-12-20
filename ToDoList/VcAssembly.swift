d7fc4cbc033b37cc7e1e10189b8e95439aba204d
Иван Алмазов <silvep4@gmail.com>
2020-12-20 12:04:55 +0300

    add EasyDi

--- ToDoList/VcAssembly.swift
+++ ToDoList/VcAssembly.swift
@@ -6,16 +6,35 @@
 //
 
 import UIKit
+import EasyDi
 
-class VcAssembly {
+//class VcAssembly {
+//    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+//
+//    var toDoViewController: ToDoViewController{
+//       let vc =  storyboard.instantiateViewController (withIdentifier: "ToDoViewController") as! ToDoViewController
+//
+//        vc.alertService = AlertServiceAssembly().firstAlertService(with: vc)
+//        vc.todo = ToDoAssembly().todoModel
+//
+//        return vc
+//    }
+//}
+
+class VcAssembly: Assembly {
     let storyboard = UIStoryboard(name: "Main", bundle: nil)
     
     var toDoViewController: ToDoViewController{
-       let vc =  storyboard.instantiateViewController (withIdentifier: "ToDoViewController") as! ToDoViewController
         
-        vc.alertService = AlertServiceAssembly().firstAlertService(with: vc)
-        vc.todo = ToDoAssembly().todoModel
+        return define(init: self.storyboard.instantiateViewController (withIdentifier: "ToDoViewController") as! ToDoViewController) {
+      
         
-        return vc
+//            $0.alertService = AlertServiceAssembly().firstAlertService(with: self.toDoViewController)
+            $0.todo = self.todo
+        }
+    }
+    
+    var todo: ToDoModel {
+        return define(init: ToDoAssembly().todoModel)
     }
 }
